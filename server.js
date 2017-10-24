3accd67469d8cc5400c2f6d5ad4cea33ca6033a0
killajk2201 <killajk2201@gmail.com>
2017-10-24 07:30:10 -0500

    Started working on the server

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+var config = require('./config');
+
 var express = require('express');
 var http = require('http');
 var app = express();
 var dist;
 dist = http.createServer(app);
+dist.listen(process.env.PORT, function(){
+    if (config.debug) console.log(process.env.PORT + ' is the magic port');
+});
